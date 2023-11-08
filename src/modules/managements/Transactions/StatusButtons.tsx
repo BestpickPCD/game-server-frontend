106e3e655e031711934a2653e71cf849a81894ac
PCD <pascalpcd@bestpicktechnology.com>
2023-11-08 15:35:09 +0700

    feature/sub-features

--- src/modules/managements/Transactions/StatusButtons.tsx
+++ src/modules/managements/Transactions/StatusButtons.tsx
@@ -1,13 +1,43 @@
 import { LoadingButton } from '@mui/lab';
-import { Grid } from '@mui/material';
+import {
+  Grid,
+  Modal,
+  Stack,
+  Button,
+  Card,
+  Box,
+  Typography,
+  TextField
+} from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useToast } from 'src/utils/hooks';
-import { useTransactionActionMutation } from 'src/services/transactionService';
-
+import {
+  useTransactionActionMutation,
+  useGetCallbackTransactionDetailsMutation
+} from 'src/services/transactionService';
 interface data {
   status: string;
   id: string;
+  method: string;
+  callbackId: string | null;
 }
+
+const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  background: 'white',
+  padding: '16px',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 1,
+  alignItems: 'center',
+  backgroundColor: 'white',
+  boxShadow: 4,
+  p: 2,
+  borderRadius: 1
+};
 
 const StatusButtons = (data: data): JSX.Element => {
   let isAdmin = false;
@@ -18,15 +48,41 @@ const StatusButtons = (data: data): JSX.Element => {
   }
 
   const { notify } = useToast();
-  const { id, status } = data;
+  const { id, status, method, callbackId } = data;
 
   const [transactionAction] = useTransactionActionMutation();
   const [loading, setLoading] = useState(false);
   const [statusTransaction, setStatus] = useState(status);
+  const [seamlessDetail, setSeamlessDetail] = useState(false);
+  const [callbackData, setCallbackData] = useState({
+    username: '',
+    amount: 0,
+    transaction: {}
+  });
 
   useEffect(() => {
     setStatus(status);
+    if (method === 'seamless') {
+      setSeamlessDetail(true);
+    }
   }, []);
+
+  const [getCallbackTransaction] = useGetCallbackTransactionDetailsMutation();
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = async () => {
+    const result = await getCallbackTransaction({ id: callbackId });
+    if ('data' in result) {
+      const { data: callbackData } = result.data;
+      setCallbackData(callbackData);
+    }
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const approveTransaction = async () => {
     setLoading(true);
@@ -100,13 +156,56 @@ const StatusButtons = (data: data): JSX.Element => {
           </Grid>
         </Grid>
       ) : (
-        <Grid container>
-          <Grid item xs={12}>
-            <LoadingButton fullWidth variant="outlined" color="success">
-              {'Approved'}
+        <Stack spacing={{ xs: 1, sm: 2 }} direction="row" flexWrap="wrap">
+          <LoadingButton
+            sx={{ flex: 1 }}
+            fullWidth
+            variant="outlined"
+            color="success"
+          >
+            {'Approved'}
+          </LoadingButton>
+
+          {seamlessDetail && (
+            <LoadingButton
+              sx={{ flex: 1 }}
+              fullWidth
+              variant="outlined"
+              color="info"
+              onClick={handleOpen}
+            >
+              {'Details'}
             </LoadingButton>
-          </Grid>
-        </Grid>
+          )}
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={modalStyle}>
+              <TextField
+                label="Username"
+                variant="outlined"
+                fullWidth
+                value={callbackData.username}
+              />
+              <TextField
+                label="Number"
+                variant="outlined"
+                fullWidth
+                value={callbackData.amount}
+              />
+              <div>
+                <p>JSON Data:</p>
+                <pre>{JSON.stringify(callbackData.transaction, null, 2)}</pre>
+              </div>
+              <Button fullWidth variant="outlined" onClick={handleClose}>
+                Close
+              </Button>
+            </Box>
+          </Modal>
+        </Stack>
       )}
     </>
   );
