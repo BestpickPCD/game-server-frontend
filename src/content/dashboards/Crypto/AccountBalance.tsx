002535201311230902675b4c089cbfac6b4bda57
nguyen truong <nguyentp@bestpicktechnology.com>
2023-12-15 16:04:17 +0700

    update dashboard

--- src/content/dashboards/Crypto/AccountBalance.tsx
+++ src/content/dashboards/Crypto/AccountBalance.tsx
@@ -1,270 +1,231 @@
-import {
-  Button,
-  Card,
-  Box,
-  Grid,
-  Typography,
-  useTheme,
-  styled,
-  Avatar,
-  Divider,
-  alpha,
-  ListItem,
-  ListItemText,
-  List,
-  ListItemAvatar
-} from '@mui/material';
-import TrendingUp from '@mui/icons-material/TrendingUp';
-import Chart from 'react-apexcharts';
-import type { ApexOptions } from 'apexcharts';
-import { useGetDashboardQuery } from 'src/services/userService';
-import { useEffect, useState } from 'react';
+import { Box, Card, Grid, Skeleton, Typography } from '@mui/material';
 import { FormattedMessage } from 'react-intl';
-
-const AvatarSuccess = styled(Avatar)(
-  ({ theme }) => `
-      background-color: ${theme.colors.success.main};
-      color: ${theme.palette.success.contrastText};
-      width: ${theme.spacing(8)};
-      height: ${theme.spacing(8)};
-      box-shadow: ${theme.colors.shadows.success};
-`
-);
-
-const ListItemAvatarWrapper = styled(ListItemAvatar)(
-  ({ theme }) => `
-  min-width: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-right: ${theme.spacing(1)};
-  padding: ${theme.spacing(0.5)};
-  border-radius: 60px;
-  background: ${
-    theme.palette.mode === 'dark'
-      ? theme.colors.alpha.trueWhite[30]
-      : alpha(theme.colors.alpha.black[100], 0.07)
-  };
-
-  img {
-    background: ${theme.colors.alpha.trueWhite[100]};
-    padding: ${theme.spacing(0.5)};
-    display: block;
-    border-radius: inherit;
-    height: ${theme.spacing(4.5)};
-    width: ${theme.spacing(4.5)};
-  }
-`
-);
+import { useGetDashboardQuery } from 'src/services/userService';
 
 const AccountBalance = (): JSX.Element => {
-  const theme = useTheme();
-
-  const { data } = useGetDashboardQuery({ refetchOnMountOrArgChange: true });
-  const chartOptions: ApexOptions = {
-    chart: {
-      background: 'transparent',
-      stacked: false,
-      toolbar: {
-        show: false
-      }
-    },
-    plotOptions: {
-      pie: {
-        donut: {
-          size: '60%'
-        }
-      }
-    },
-    colors: ['#ff9900', '#1c81c2', '#333', '#5c6ac0'],
-    dataLabels: {
-      enabled: true,
-      formatter: function (val) {
-        return val + '%';
-      },
-      style: {
-        colors: [theme.colors.alpha.trueWhite[100]]
-      },
-      background: {
-        enabled: true,
-        foreColor: theme.colors.alpha.trueWhite[100],
-        padding: 8,
-        borderRadius: 4,
-        borderWidth: 0,
-        opacity: 0.3,
-        dropShadow: {
-          enabled: true,
-          top: 1,
-          left: 1,
-          blur: 1,
-          color: theme.colors.alpha.black[70],
-          opacity: 0.5
-        }
-      },
-      dropShadow: {
-        enabled: true,
-        top: 1,
-        left: 1,
-        blur: 1,
-        color: theme.colors.alpha.black[50],
-        opacity: 0.5
-      }
-    },
-    fill: {
-      opacity: 1
-    },
-    labels: ['Bitcoin', 'Ripple', 'Cardano', 'Ethereum'],
-    legend: {
-      labels: {
-        colors: theme.colors.alpha.trueWhite[100]
-      },
-      show: false
-    },
-    stroke: {
-      width: 0
-    },
-    theme: {
-      mode: theme.palette.mode
-    }
-  };
-
-  const chartSeries = [10, 20, 25, 45];
+  const { data, isFetching } = useGetDashboardQuery(
+    {},
+    { refetchOnMountOrArgChange: true }
+  );
 
   return (
-    <Card>
-      <Grid spacing={0} container>
-        <Grid item xs={12} md={6}>
-          <Box p={4}>
+    <Box>
+      <Grid
+        container
+        columns={{ xs: 4, sm: 8, md: 12, lg: 16 }}
+        pt={4}
+        pb={2}
+        spacing={2}
+      >
+        <Grid item xs={4} sm={4} md={4} lg={4}>
+          <Card sx={{ p: 2 }}>
             <Typography
-              sx={{
-                pb: 3
-              }}
-              variant="h4"
+              variant="h5"
+              width="90%"
+              overflow="hidden"
+              textOverflow="ellipsis"
+              whiteSpace="nowrap"
+              pb={1}
             >
-              <FormattedMessage id="label.account-balance" />
+              <FormattedMessage id="title.current.holding.amount" />
             </Typography>
-            <Box>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+            >
               <Typography variant="h1" gutterBottom>
-                {data?.balance.balance} {data?.currency.code}
+                {isFetching ? (
+                  <Skeleton animation="wave" height={44} width={100} />
+                ) : (
+                  data?.balance.balance
+                )}
               </Typography>
-              <Typography
-                variant="h4"
-                fontWeight="normal"
-                color="text.secondary"
-              >
-                {data?.balance.balance} {data?.currency.code}
-              </Typography>
-              {/* <Box
-                display="flex"
-                sx={{
-                  py: 4
-                }}
-                alignItems="center"
-              >
-                <AvatarSuccess
-                  sx={{
-                    mr: 2
-                  }}
-                  variant="rounded"
-                >
-                  <TrendingUp fontSize="large" />
-                </AvatarSuccess>
-                <Box>
-                  <Typography variant="h4">+ $3,594.00</Typography>
-                  <Typography variant="subtitle2" noWrap>
-                    this month
-                  </Typography>
-                </Box>
-              </Box> */}
+              {isFetching ? (
+                <Skeleton animation="wave" height={44} width={40} />
+              ) : (
+                <Typography variant="h5">POT</Typography>
+              )}
             </Box>
-            {/* <Grid container spacing={3}>
-              <Grid sm item>
-                <Button fullWidth variant="outlined">
-                  Send
-                </Button>
-              </Grid>
-              <Grid sm item>
-                <Button fullWidth variant="contained">
-                  Receive
-                </Button>
-              </Grid>
-            </Grid> */}
-          </Box>
+          </Card>
         </Grid>
-        {/* <Grid
-          sx={{
-            position: 'relative'
-          }}
-          display="flex"
-          alignItems="center"
-          item
-          xs={12}
-          md={6}
-        >
-          <Box
-            component="span"
-            sx={{
-              display: { xs: 'none', md: 'inline-block' }
-            }}
-          >
-            <Divider absolute orientation="vertical" />
-          </Box>
-          <Box py={4} pr={4} flex={1}>
-            <Grid container spacing={0}>
-              <Grid
-                xs={12}
-                sm={5}
-                item
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Chart
-                  height={250}
-                  options={chartOptions}
-                  series={chartSeries}
-                  type="donut"
-                />
-              </Grid>
-              <Grid xs={12} sm={7} item display="flex" alignItems="center">
-                <List
-                  disablePadding
-                  sx={{
-                    width: '100%'
-                  }}
-                >
-                  {chartSeries.map((item, index) => (
-                    <ListItem disableGutters>
-                      <ListItemAvatarWrapper>
-                        <img
-                          alt="BTC"
-                          src="/static/images/placeholders/logo/bitcoin.png"
-                        />
-                      </ListItemAvatarWrapper>
-                      <ListItemText
-                        primary="BTC"
-                        primaryTypographyProps={{ variant: 'h5', noWrap: true }}
-                        secondary="Bitcoin"
-                        secondaryTypographyProps={{
-                          variant: 'subtitle2',
-                          noWrap: true
-                        }}
-                      />
-                      <Box>
-                        <Typography align="right" variant="h4" noWrap>
-                          20%
-                        </Typography>
-                        <span color="success">+2.54%</span>
-                      </Box>
-                    </ListItem>
-                  ))}
-                </List>
-              </Grid>
-            </Grid>
-          </Box>
-        </Grid> */}
+        <Grid item xs={4} sm={4} md={4} lg={4}>
+          <Card sx={{ p: 2 }}>
+            <Typography
+              variant="h5"
+              width="90%"
+              overflow="hidden"
+              textOverflow="ellipsis"
+              whiteSpace="nowrap"
+              pb={1}
+            >
+              <FormattedMessage id="title.sub.agent.current.total.holding.amount" />
+            </Typography>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              {isFetching ? (
+                <Skeleton animation="wave" height={44} width={100} />
+              ) : (
+                <Typography variant="h1" gutterBottom>
+                  {data?.balanceOfChildAgents}
+                </Typography>
+              )}
+              {isFetching ? (
+                <Skeleton animation="wave" height={44} width={40} />
+              ) : (
+                <Typography variant="h5">POT</Typography>
+              )}
+            </Box>
+          </Card>
+        </Grid>
+        <Grid item xs={4} sm={4} md={4} lg={4}>
+          <Card sx={{ p: 2 }}>
+            <Typography
+              variant="h5"
+              width="90%"
+              overflow="hidden"
+              textOverflow="ellipsis"
+              whiteSpace="nowrap"
+              pb={1}
+            >
+              <FormattedMessage id="title.current.total.amount.held.by.sub.users" />
+            </Typography>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              {isFetching ? (
+                <Skeleton animation="wave" height={44} width={100} />
+              ) : (
+                <Typography variant="h1" gutterBottom>
+                  {data?.balanceOfChildUsers}
+                </Typography>
+              )}
+              {isFetching ? (
+                <Skeleton animation="wave" height={44} width={40} />
+              ) : (
+                <Typography variant="h5">POT</Typography>
+              )}
+            </Box>
+          </Card>
+        </Grid>
       </Grid>
-    </Card>
+      <Grid container columns={{ xs: 4, sm: 8, md: 12, lg: 16 }} spacing={2}>
+        <Grid item xs={4} sm={4} md={4} lg={4}>
+          <Card sx={{ p: 2 }}>
+            <Typography
+              variant="h5"
+              width="90%"
+              overflow="hidden"
+              textOverflow="ellipsis"
+              whiteSpace="nowrap"
+              pb={1}
+            >
+              <FormattedMessage id="title.total.number.of.subagents" />
+            </Typography>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              {isFetching ? (
+                <Skeleton animation="wave" height={44} width={100} />
+              ) : (
+                <Typography variant="h1" gutterBottom>
+                  {data?.totalSubAgent}
+                </Typography>
+              )}
+            </Box>
+          </Card>
+        </Grid>
+        <Grid item xs={4} sm={4} md={4} lg={4}>
+          <Card sx={{ p: 2 }}>
+            <Typography
+              variant="h5"
+              width="90%"
+              overflow="hidden"
+              textOverflow="ellipsis"
+              whiteSpace="nowrap"
+              pb={1}
+            >
+              <FormattedMessage id="title.number.of.distribution.sub.agents" />
+            </Typography>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              {isFetching ? (
+                <Skeleton animation="wave" height={44} width={100} />
+              ) : (
+                <Typography variant="h1" gutterBottom>
+                  {data?.distributionSubAgent}
+                </Typography>
+              )}
+            </Box>
+          </Card>
+        </Grid>
+        <Grid item xs={4} sm={4} md={4} lg={4}>
+          <Card sx={{ p: 2 }}>
+            <Typography
+              variant="h5"
+              width="90%"
+              overflow="hidden"
+              textOverflow="ellipsis"
+              whiteSpace="nowrap"
+              pb={1}
+            >
+              <FormattedMessage id="title.number.of.operations.sub.agents" />
+            </Typography>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              {' '}
+              {isFetching ? (
+                <Skeleton animation="wave" height={44} width={100} />
+              ) : (
+                <Typography variant="h1" gutterBottom>
+                  {data?.operationSubAgent}
+                </Typography>
+              )}
+            </Box>
+          </Card>
+        </Grid>
+        <Grid item xs={4} sm={4} md={4} lg={4}>
+          <Card sx={{ p: 2 }}>
+            <Typography
+              variant="h5"
+              width="90%"
+              overflow="hidden"
+              textOverflow="ellipsis"
+              whiteSpace="nowrap"
+              pb={1}
+            >
+              <FormattedMessage id="title.number.of.parallel.sub.agents" />
+            </Typography>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              {isFetching ? (
+                <Skeleton animation="wave" height={44} width={100} />
+              ) : (
+                <Typography variant="h1" gutterBottom>
+                  {data?.parallelSubAgent}
+                </Typography>
+              )}
+            </Box>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
