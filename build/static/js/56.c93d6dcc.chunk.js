3447bb53bc221099b9170e569217a494d7d1dc93
PCD <pascalpcd@bestpicktechnology.com>
2024-01-17 15:20:53 +0700

    build folder

---
+++ build/static/js/56.c93d6dcc.chunk.js
@@ -0,0 +1,2 @@
+(self.webpackChunkadmin_dashboard=self.webpackChunkadmin_dashboard||[]).push([[56],{50864:(a,s,d)=>{"use strict";d.r(s),d.d(s,{default:()=>c});var n=d(4811),o=(d(43209),d(80184));const c=()=>(0,o.jsx)(n.Z,{url:"".concat(window.location.origin,"/swagger/api-docs.yaml")})},24654:()=>{}}]);
+//# sourceMappingURL=56.c93d6dcc.chunk.js.map
